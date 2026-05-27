52b88206e9f4bcd57d206db014bcfd4663959aa9
BoicoMihai <b.sasa50@gmail.com>
2026-05-27 11:36:53 +0300

    Added task 2 to test php file

--- test.php
+++ test.php
@@ -9,5 +9,25 @@
     <h1><?php echo "Hello World!"; ?></h1>
 
     <?php echo "<script> console.log('Hello world!'); </script>"; ?>
+
+    <?php
+        $numere = [12, 7, 5, 18, 22, 9, 14, 3, 8, 11];
+        $pare = 0;
+        $impare = 0;
+
+        for ($i = 0; $i < count($numere); $i++) {
+
+            if ($numere[$i] % 2 == 0) {
+                echo $numere[$i] . " este par<br>";
+                $pare++;
+            } else {
+                echo $numere[$i] . " este impar<br>";
+                $impare++;
+            }
+        }
+
+        echo "<br>Total numere pare: " . $pare . "<br>";
+        echo "Total numere impare: " . $impare;
+    ?>
 </body>
 </html>
